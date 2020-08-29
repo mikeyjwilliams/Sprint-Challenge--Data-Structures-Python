a9070f30d6d63f91087bdc59a2ef1bd34bcefa2d
Mikey Williams <manicdesigner@gmail.com>
2020-08-29 14:10:19 -0400

    pseudo code thoguhts on append for ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -2,8 +2,21 @@ class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
 
+        #! I think some other datastructure
+        #! is needed to hold the nodes for
+        #! the buffer.
+
     def append(self, item):
-        pass
+        pass #! just pseudo code for now.
+        # before appending... check
+        #! if capacity has space
+            #* append target item
+        #! else
+            #* remove 1st item from capacity
+            #* then append target item to tail
+
+
+
 
     def get(self):
         pass
